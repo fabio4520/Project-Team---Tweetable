594196329e416e7c64aa9ebd133d0babba28ebe4
Enrique Vizarra <vizarramattay@gmail.com>
2022-05-04 17:37:42 -0500

    create validations to like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ApplicationRecord
-  
+  # validations
+  validates :user, uniqueness: { scope: :tweet, message: "and Tweet combination already taken" }
   # associations
   belongs_to :user
   belongs_to :tweet
