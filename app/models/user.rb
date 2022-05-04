537ab6b4904aaa54ed17933130d8f8618f16a7bc
Frank Cano <frank.cano@utec.edu.pe>
2022-05-04 16:17:48 -0500

    Add: users validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validations
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  has_one_attached :cover
+  # Associations
+  has_many :tweets, dependent: :nullify
+  has_many :likes, dependent: :destroy
+  has_many :tweets, through: :likes, counter_cache: true
+
+  # enum
+  enum role: { member: 0,
+               admin: 1}
+
 end
