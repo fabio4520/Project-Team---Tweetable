67847f324c08f7f008c467558e4a02b85e824151
fabio4520 <fabioleofc@gmail.com>
2022-05-04 17:10:45 -0500

    ADD: Associations and validations for Teets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,10 @@
 class Tweet < ApplicationRecord
+  # Associations
   belongs_to :user
-  belongs_to :replied_to
+  belongs_to :replied_to,  class_name: "Tweet", optional: true, counter_cache: true
+  has_many :likes, dependent: :nullify
+  has_many :users, through: :likes
+
+  # Validations
+  validates :body, presence: true, length: { maximum: 140 }
 end
