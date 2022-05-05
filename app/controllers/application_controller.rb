3e7bb77cb8dccec1b05120af8b49ff6d9f2f8405
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-05-04 21:22:54 -0500

    CHOR: Add configure to device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar])
+  end
+
 end
