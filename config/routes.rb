442d82a4d050ec26c4bddaeb78844aeeca289345
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-05-04 22:12:31 -0500

    CHOR: Add routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: :callbacks}
+  
+  get "/profile", to: "users#show", as: "user"
+  root "tweets#index"
+
   resources :likes
   resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
