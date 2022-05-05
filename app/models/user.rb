62a234f1c23cee54f793b420dc042f9fe2f94b0e
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-05-04 22:10:20 -0500

    CHOR: Add method to omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,15 @@ class User < ApplicationRecord
   # enum
   enum role: { member: 0,
                admin: 1}
-
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.username = auth.info.name
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
