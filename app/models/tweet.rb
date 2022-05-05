6ba1bda354637d85f51f9a0f561f6b5349e3ddf4
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-05-05 10:29:42 -0500

    FIX: inverse of name

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :replied_to, class_name: "Tweet", optional: true, counter_cache: :replies_count
   has_many :threads, class_name: "Tweet", foreign_key: "replied_to_id",
-            inverse_of: "parent", dependent: :nullify
+            inverse_of: "replied_to", dependent: :nullify
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
 
